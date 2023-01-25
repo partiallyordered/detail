e6cdc7bcd50157a5d45803ea8b1ce9f7146e4307
Matt Kingston <mattkingston@protonmail.com>
2023-01-25 19:43:48 +0100

    Super-simple implementation listing current working directory contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+use tokio::fs::read_dir;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("Failed to read directory contents {0}")]
+    FailedToReadDirContents(String, std::io::Error),
+}
+type Result<T> = std::result::Result<T, Error>;
+
+#[tokio::main]
+async fn main() -> ExitCode {
+    match _main().await {
+        Err(_) => ExitCode::FAILURE,
+        Ok(_) => ExitCode::SUCCESS
+    }
+}
+
+async fn _main() -> Result<()> {
+    let path = ".";
+    let mut dir_entries = read_dir(path).await.map_err(|e| Error::FailedToReadDirContents(path.to_string(), e))?;
+    while let Some(dir_entry) = dir_entries.next_entry().await.map_err(|e| Error::FailedToReadDirContents(path.to_string(), e))? {
+        println!("{:?}", dir_entry);
+    }
+    Ok(())
 }
