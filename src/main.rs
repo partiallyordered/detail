cd816c1af0b3337c7b03e2e4663c54eeaca63ec8
Matt Kingston <mattkingston@protonmail.com>
2023-01-25 20:18:25 +0100

    Streaming implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::process::ExitCode;
-use tokio::fs::read_dir;
+use tokio::fs::{read_dir, DirEntry};
 use thiserror::Error;
+use futures::stream::FuturesOrdered;
+use futures::StreamExt;
 
 #[derive(Error, Debug)]
 enum Error {
@@ -19,9 +21,30 @@ async fn main() -> ExitCode {
 
 async fn _main() -> Result<()> {
     let path = ".";
+    let mut stream = FuturesOrdered::new();
     let mut dir_entries = read_dir(path).await.map_err(|e| Error::FailedToReadDirContents(path.to_string(), e))?;
-    while let Some(dir_entry) = dir_entries.next_entry().await.map_err(|e| Error::FailedToReadDirContents(path.to_string(), e))? {
-        println!("{:?}", dir_entry);
+    print!("[");
+    if let Some(dir_entry) = dir_entries.next_entry().await.map_err(|e| Error::FailedToReadDirContents(path.to_string(), e))? {
+        stream.push_back(
+            tokio::spawn(async move {
+                process_dir_entry(dir_entry).await
+            })
+        );
+        while let Some(dir_entry) = dir_entries.next_entry().await.map_err(|e| Error::FailedToReadDirContents(path.to_string(), e))? {
+            stream.push_back(
+                tokio::spawn(async move {
+                    process_dir_entry(dir_entry).await.map(|s| format!(",{s}"))
+                })
+            );
+        }
     }
+    while let Some(res) = stream.next().await {
+        print!("{:?}", res);
+    }
+    print!("]");
     Ok(())
+}
+
+async fn process_dir_entry(entry: DirEntry) -> Result<String> {
+    Ok(format!("{:?}", entry))
 }
